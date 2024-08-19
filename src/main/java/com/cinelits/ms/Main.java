e44b55800d4d5755e0daf987c6ded892c859bb89
Douglas Bello <dgbello.dev@hotmail.com>
2024-08-19 11:13:16 -0300

    problem resolved: palindrome

--- src/main/java/com/cinelits/ms/Main.java
+++ src/main/java/com/cinelits/ms/Main.java
@@ -1,14 +1,9 @@
 package com.cinelits.ms;
 
-import com.cinelits.ms.twosum.TwoSum;
-
-import java.util.Arrays;
+import com.cinelits.ms.palindrome.Palindrome;
 
 public class Main {
     public static void main(String[] args) {
-        int[] input = {3, 3};
-        int target = 6;
-
-        System.out.println(Arrays.toString(TwoSum.twoSum(input, target)));
+        System.out.println(Palindrome.isPalindrome(10));
     }
 }
---
+++ src/main/java/com/cinelits/ms/palindrome/Palindrome.java
@@ -0,0 +1,18 @@
+package com.cinelits.ms.palindrome;
+
+public class Palindrome {
+    public static boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        String y = String.valueOf(x);
+        String reversed = "";
+
+        for (int i = 0; i < y.length(); i++) {
+            int lastIndex = y.length() -i - 1;
+
+            reversed = reversed.concat(String.valueOf(y.charAt(lastIndex)));
+        }
+
+        return reversed.equals(y);
+    }
+}
